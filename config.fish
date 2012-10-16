dd5f284be47c5dbfe04071018482f5a9f73ea114
Nate Soares <nate@so8r.es>
2012-10-16 09:35:02 -0700

    added more to git prompt

--- config.fish
+++ config.fish
@@ -5,9 +5,10 @@
 #  can't multiline in (parens)
 set fish_greeting ''
 
-set -g __prompt_fg_sep (set_color normal)
+set -g __prompt_fg_sep (set_color cyan)
 set -g __prompt_fg_mux (set_color blue)
 set -g __prompt_fg_user (set_color magenta)
+set -g __prompt_fg_at (set_color white)
 set -g __prompt_fg_host (set_color yellow)
 set -g __prompt_fg_path (set_color green)
 set -g __prompt_fg_branch (set_color magenta)
@@ -26,7 +27,7 @@ set -g __prompt_reset (set_color normal)
 set -g __prompt_host (hostname|cut -d . -f 1)
 
 
-function __prompt_mux
+function __prompt_tmux
 	if test $TERM != tmux; return; end
 	set session (tmux display-message -p "#S")
 	if test $session != 0
@@ -43,6 +44,19 @@ function __prompt_statuschar
 	and echo -n $__prompt_fg_branch$argv[2]
 end
 
+function __prompt_git_location
+	set -l branch (git symbolic-ref HEAD ^/dev/null|cut -d '/' -f 3)
+	set -l tag (git describe --tags --exact-match HEAD ^/dev/null)
+	if test -z "$branch$tag"
+		echo -n $__prompt_fg_branch' <detached> '
+		return
+	end
+	echo -n $__prompt_fg_sep' on '
+	test -n "$branch"; and echo -n $__prompt_fg_branch$branch
+	test -n "$tag"; and echo -n $__prompt_fg_sep'#'$__prompt_fg_branch$tag;
+	echo -n ' '
+end
+
 function __prompt_git_status
 	set -l gs (git status --porcelain ^/dev/null|cut -c 1-2|sort|tr -d '\n')
 
@@ -54,14 +68,14 @@ function __prompt_git_status
 	echo $gs|grep -qe "^\(..\)*??"; and set unstaged
 
 	# Split up the index flags and the staging flags
-	set -g __prompt_gs_indexed (echo $gs|sed -e "s/\(.\).\?/\1/g"|tr -d '?U'|tr ' ' '_')
-	set -g __prompt_gs_staged (echo $gs|sed -e "s/.\(.\?\)/\1/g"|tr -d '?U'|tr ' ' '_')
+	set -g __prompt_gs_indexed (echo $gs|sed -e "s/\(.\).\?/\1/g"|tr -d '?U')
+	set -g __prompt_gs_staged (echo $gs|sed -e "s/.\(.\?\)/\1/g"|tr -d '?U')
 
-	__prompt_statuschar 'M' '*' 
-	__prompt_statuschar 'A' '+' 
-	__prompt_statuschar 'R' '→' 
-	__prompt_statuschar 'C' '⇒' 
-	__prompt_statuschar 'D' '✗' 
+	__prompt_statuschar 'M' '*'
+	__prompt_statuschar 'A' '+'
+	__prompt_statuschar 'R' '→'
+	__prompt_statuschar 'C' '⇒'
+	__prompt_statuschar 'D' '✗'
 
 	set -e __prompt_gs_indexed
 	set -e __prompt_gs_staged
@@ -73,25 +87,25 @@ end
 function fish_prompt
 	set laststatus $status
 	# Stick in some multiplexor state
-	__prompt_mux
+	__prompt_tmux
 	# Username
-	echo -n $__prompt_fg_user(whoami)$__prompt_fg_sep'@'
+	echo -n $__prompt_fg_user(whoami)$__prompt_fg_at'@'
 	# Host
 	echo -n $__prompt_fg_host$__prompt_host$__prompt_fg_sep':'
 	# Location
 	echo -n $__prompt_fg_path(pwd|sed -e "s,^$HOME,~,")
 	# Become aware of the repository
-	set branch (git symbolic-ref HEAD ^/dev/null|cut -d '/' -f 3)
-	test -n "$branch";
-	and echo -n $__prompt_fg_path' on '
-	and echo -n $__prompt_fg_branch$branch' '
+	set -l gitdir (git rev-parse --git-dir ^/dev/null)
+	test -n "$gitdir";
+	and __prompt_git_location
+	# Consider __prompt_git_upstream here when you know what you're doing
 	and __prompt_git_status
 	# Multi-lineification & status color
 	test $laststatus -ne 0
 	and echo $__prompt_fg_bad
 	or echo $__prompt_fg_prompt
 	# Repository aware prompt character
-	test -n "$branch"; and echo -n »; or echo -n →
+	test -n "$gitdir"; and echo -n »; or echo -n →
 	# Prep for user input
 	echo -n $__prompt_reset' '
 end
